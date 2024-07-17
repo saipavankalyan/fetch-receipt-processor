263e7a1d7291441e00550ad2070f5652920a1f5b
saipavankalyan <saipavan.munaga@gmail.com>
2024-07-16 22:50:42 -0400

    Added Logging

--- src/main/java/com/fetch/receipt_processor/controller/ReceiptController.java
+++ src/main/java/com/fetch/receipt_processor/controller/ReceiptController.java
@@ -6,6 +6,8 @@ import com.fetch.receipt_processor.dto.ReceiptRequestDto;
 import com.fetch.receipt_processor.exception.ReceiptNotFoundException;
 import com.fetch.receipt_processor.service.ReceiptService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +19,21 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ReceiptController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReceiptController.class);
     private ReceiptService receiptService;
 
     @PostMapping(value = "/process")
     @ResponseStatus(HttpStatus.OK)
     public ReceiptProcessResponseDto processReceipt(@RequestBody ReceiptRequestDto request) {
         UUID uuid = receiptService.addReceipt(request);
+        LOGGER.info("Receipt [id = {}] added to the system", uuid.toString());
         return ReceiptProcessResponseDto.builder().id(uuid.toString()).build();
     }
 
     @GetMapping("/{receiptId}/points")
     public ReceiptPointsDto getPoints(@PathVariable("receiptId") UUID receiptId) throws ReceiptNotFoundException {
         Integer points = receiptService.calculatePoints(receiptId);
+        LOGGER.info("Calculation of points for Receipt [id = {}] resulted in [ {} ] points", receiptId.toString(), points);
         return ReceiptPointsDto.builder().points(points).build();
     }
 
--- src/main/java/com/fetch/receipt_processor/service/ReceiptService.java
+++ src/main/java/com/fetch/receipt_processor/service/ReceiptService.java
@@ -8,6 +8,8 @@ import com.fetch.receipt_processor.mappers.ReceiptMapper;
 import com.fetch.receipt_processor.repository.ReceiptRepository;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.CharUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -23,19 +25,24 @@ public class ReceiptService {
 
     private static final LocalTime START_TIME = LocalTime.parse("14:00", TIME_FORMAT);
     private static final LocalTime END_TIME = LocalTime.parse("16:00", TIME_FORMAT);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReceiptService.class);
     private ReceiptMapper receiptMapper;
     private ReceiptRepository receiptRepository;
 
     public UUID addReceipt(ReceiptRequestDto receiptRequest) {
         final ReceiptDbo receiptDbo = receiptMapper.map(receiptRequest);
+        LOGGER.debug("Receipt Dbo Mapped from Receipt Dto.");
         receiptDbo.getItems().forEach(itemDbo -> itemDbo.setReceipt(receiptDbo));
         ReceiptDbo savedReceiptDbo = receiptRepository.save(receiptDbo);
+        LOGGER.debug("Receipt [id = {}] saved tp DB", savedReceiptDbo.getId());
         return savedReceiptDbo.getId();
     }
 
     public Integer calculatePoints(UUID receiptId) throws ReceiptNotFoundException {
         ReceiptDbo receiptDbo = receiptRepository.findById(receiptId)
                 .orElseThrow(() -> new ReceiptNotFoundException(receiptId));
+
+        LOGGER.debug("Receipt [id = {}] Fetched from DB.", receiptId);
 
         Integer points = 0;
         // Rule - One point for every alphanumeric character in the retailer name.
@@ -45,18 +52,27 @@ public class ReceiptService {
             }
         }
 
+        LOGGER.debug("Rule 1 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
+
         // Rule - 50 points if the total is a round dollar amount with no cents.
         if (receiptDbo.getTotal().remainder(BigDecimal.ONE).setScale(0, RoundingMode.CEILING).equals(BigDecimal.ZERO)) {
             points += 50;
         }
+
+        LOGGER.debug("Rule 2 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
 
         // Rule - 25 points if the total is a multiple of 0.25.
         if (receiptDbo.getTotal().multiply(BigDecimal.valueOf(100)).remainder(BigDecimal.valueOf(25)).setScale(0, RoundingMode.CEILING).equals(BigDecimal.ZERO)) {
             points += 25;
         }
 
+        LOGGER.debug("Rule 3 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
+
         // Rule - 5 points for every two items on the receipt.
         points += (receiptDbo.getItems().size() / 2) * 5;
+
+        LOGGER.debug("Rule 4 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
+
 
         // Rule - If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
         for (ItemDbo item : receiptDbo.getItems()) {
@@ -65,15 +81,21 @@ public class ReceiptService {
             }
         }
 
+        LOGGER.debug("Rule 5 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
+
         // Rule - 6 points if the day in the purchase date is odd.
         if (receiptDbo.getPurchaseDate().getDate() % 2 == 1) {
             points += 6;
         }
 
+        LOGGER.debug("Rule 6 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
+
         // Rule - 10 points if the time of purchase is after 2:00pm and before 4:00pm.
         if (receiptDbo.getPurchaseTime().isAfter(START_TIME) && receiptDbo.getPurchaseTime().isBefore(END_TIME)) {
             points += 10;
         }
+
+        LOGGER.debug("Rule 7 process resulted [ {} ] points for receipt [ id = {} ]", points, receiptId);
 
         return points;
     }
